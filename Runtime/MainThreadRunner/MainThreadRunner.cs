75abf3edeedcc19b568c86b7e2fa5e4ed6f30de7
gluks <gluk-02@yandex.ru>
2026-06-17 09:36:00 +0500

    Update MainThreadRunner.cs

--- Runtime/MainThreadRunner/MainThreadRunner.cs
+++ Runtime/MainThreadRunner/MainThreadRunner.cs
@@ -6,25 +6,38 @@ namespace MiniIT.Utils
 {
 	public class MainThreadRunner : IMainThreadRunner
 	{
+		private readonly int _mainThreadId;
+		private readonly SynchronizationContext _synchronizationContext;
 		private readonly TaskScheduler _scheduler;
 
 		public MainThreadRunner()
 		{
-			_scheduler = SynchronizationContext.Current != null ?
+			_mainThreadId = Thread.CurrentThread.ManagedThreadId;
+			_synchronizationContext = SynchronizationContext.Current;
+			_scheduler = _synchronizationContext != null ?
 				TaskScheduler.FromCurrentSynchronizationContext() :
 				TaskScheduler.Current;
 		}
 
 		public void RunInMainThread(Action action)
 		{
-			if (TaskScheduler.Current == _scheduler)
+			if (Thread.CurrentThread.ManagedThreadId == _mainThreadId)
 			{
 				action();
+			}
+			else if (_synchronizationContext != null)
+			{
+				_synchronizationContext.Post(RunAction, action);
 			}
 			else
 			{
 				_ = Task.Factory.StartNew(action, CancellationToken.None, TaskCreationOptions.DenyChildAttach, _scheduler);
 			}
+		}
+
+		private static void RunAction(object state)
+		{
+			((Action)state).Invoke();
 		}
 	}
 }
